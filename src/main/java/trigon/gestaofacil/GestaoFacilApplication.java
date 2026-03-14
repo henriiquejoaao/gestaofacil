75a757f6cfe67f9c2b57a8803f7a138c8d6319f3
João Henrique <joaotrindadeguimaraes@gmail.com>
2026-03-14 14:45:46 -0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/trigon/gestaofacil/GestaoFacilApplication.java
@@ -0,0 +1,13 @@
+package trigon.gestaofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFacilApplication.class, args);
+	}
+
+}
