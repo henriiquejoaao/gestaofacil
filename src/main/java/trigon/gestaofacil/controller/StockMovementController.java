f1b19f80e517dd1ba4de57cf969d879861daccf0
henriiquejoaao <joaotrindadeguimaraes@gmail.com>
2026-06-22 18:15:21 -0300

    feat: add stock movement controller

---
+++ src/main/java/trigon/gestaofacil/controller/StockMovementController.java
@@ -0,0 +1,49 @@
+package trigon.gestaofacil.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import trigon.gestaofacil.dto.StockMovementResponseDTO;
+import trigon.gestaofacil.service.StockMovementService;
+
+@RestController
+@RequestMapping("/stock-movements")
+class StockMovementController {
+
+  private final StockMovementService service;
+
+  public StockMovementController(StockMovementService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<StockMovementResponseDTO>> getAll() {
+    return ResponseEntity.ok(service.getAll());
+  }
+
+  @GetMapping("/latest")
+  public ResponseEntity<List<StockMovementResponseDTO>> getLatest() {
+    return ResponseEntity.ok(service.getLatest());
+  }
+
+  @GetMapping("/today")
+  public ResponseEntity<List<StockMovementResponseDTO>> getToday() {
+    return ResponseEntity.ok(service.getToday());
+  }
+
+  @GetMapping("/today/count")
+  public ResponseEntity<Long> countToday() {
+    return ResponseEntity.ok(service.countToday());
+  }
+
+  @GetMapping("/product/{productId}")
+  public ResponseEntity<List<StockMovementResponseDTO>> getByProduct(@PathVariable UUID productId) {
+    return ResponseEntity.ok(service.getByProduct(productId));
+  }
+}
